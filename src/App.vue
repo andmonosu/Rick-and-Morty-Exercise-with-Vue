4d5363aecacc1bfe797811520fbc11a30c4ca5a5
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-03-03 11:55:59 +0100

    Project created

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+  </header>
+  <main>
+
+  </main>
+</template>
+<script lang="js">
+export default {
+  methods: {
+
+  },computed: {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
