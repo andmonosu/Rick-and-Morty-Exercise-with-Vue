909f8238f1dfbbc353a0b08f26297c0aca4466e6
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-03-15 17:16:15 +0100

    Revert "Added dist for deploy"
    This reverts commit 09fd71222a99650f92f3f29ed6649ee554be8194.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/Rick-and-Morty-Exercise-with-Vue/',
+  base: '/Rick-and-Morty-Exercise-with-Vue/',
   plugins: [vue()],
   resolve: {
     alias: {
