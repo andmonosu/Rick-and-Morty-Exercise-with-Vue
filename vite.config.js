46a5ef500a9bf19b115afb4ce82500409b73b3bd
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-03-15 16:23:53 +0100

    Added dist for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  publicPath: "/Rick-and-Morty-Exercise-with-Vue/",
+module.exports={
+  publicPath: '/Rick-and-Morty-Exercise-with-Vue/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}
