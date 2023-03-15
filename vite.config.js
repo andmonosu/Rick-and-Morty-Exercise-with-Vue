3521154c5fbaf9512060db7a3e71959033b9bd2b
Andreu Montagut Osuna <80252401+andmonosu@users.noreply.github.com>
2023-03-15 18:23:58 +0100

    Update vite.config.js
    Modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-module.exports={
-  publicPath: '/Rick-and-Morty-Exercise-with-Vue/',
+export default defineConfig({
+  base: '/Rick-and-Morty-Exercise-with-Vue/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-}
+})
