9ec329bc993c5b86192b9f20ebac518bff3846c0
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-03-15 15:56:39 +0100

    Added vist for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Rick-and-Morty-Exercise-with-Vue',
+  base: '/Rick-and-Morty-Exercise-with-Vue/dist/s',
   plugins: [vue()],
   resolve: {
     alias: {
